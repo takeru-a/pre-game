3f3a0d4b0577e650e6fd4eded08307c148b3357f
takeru-a <pypy20191022@gmail.com>
2021-10-26 01:02:49 +0900

    背景のスクロール機能、ユーザインターフェイスの追加

--- enemy.py
+++ enemy.py
@@ -6,7 +6,8 @@ class Enemy():
     eximg = pg.image.load("./imgs/explosion.png")
     img_w = img.get_width()
     img_h = img.get_height()
-    def __init__(self,x ,y=np.random.randint(0,481-img_h)):
+    HEIGHT = 481
+    def __init__(self,x ,y=np.random.randint(0,HEIGHT-img_h)):
         self.x = x
         self.y = y
         self.first = self.x
@@ -14,7 +15,7 @@ class Enemy():
         self.x -= speed
         if self.x <= -1*self.img_w:
             self.x = self.first
-            self.y = np.random.randint(0,481-self.img_h)
+            self.y = np.random.randint(0,self.HEIGHT-self.img_h)
             
     def getImage(self):
         return self.img
@@ -25,6 +26,6 @@ class Enemy():
         point = [self.x,self.y]
         return point
     def hit(self):
-        num = np.random.randint(0,481-self.img_h)
+        num = np.random.randint(0,self.HEIGHT-self.img_h)
         self.setPoint(num)
         return self.eximg
--- item.py
+++ item.py
@@ -2,11 +2,12 @@ import pygame as pg
 import numpy as np
 
 class Item():
+    HEIGHT = 481
     img = pg.image.load("./imgs/heart.png")
     img = pg.transform.scale(img,(50,50))
     img_w = img.get_width()
     img_h = img.get_height()
-    def __init__(self,x ,y=np.random.randint(0,481-img_h)):
+    def __init__(self,x ,y=np.random.randint(0,HEIGHT-img_h)):
         self.x = x
         self.y = y
         self.first = self.x
@@ -17,7 +18,7 @@ class Item():
         return self.img
     def setPoint(self):
         self.x = self.first
-        self.y = np.random.randint(0,481-self.img_h)
+        self.y = np.random.randint(0,self.HEIGHT-self.img_h)
     def getPoint(self):
         point = [self.x,self.y]
         return point
